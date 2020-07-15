1207a1d152b9455b5fe20f8fd976b0a6e364e834
ClaysHackers <68355091+ClaysHackers@users.noreply.github.com>
2020-07-15 18:16:04 -0400

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Script para rootear
+
+echo Instalación de varios paquetes
+
+sleep 1s
+
+cd /home/anonymous
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install transmision gedit gimp chromium-browser vlc
